03e58dbe579273cba0f5ef188535754d69d62bb1
Torben Jost <torbenjost@proton.me>
2024-11-26 16:20:04 +0100

    create class Book

---
+++ 261124_package/codeSession/src/main/java/de/neuefische/car/Book.java
@@ -0,0 +1,32 @@
+package de.neuefische.car;
+
+public class Book {
+    private String title;
+    private String author;
+    private int isbn;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+}
