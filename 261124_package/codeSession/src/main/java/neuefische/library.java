4a777e2c0ba02f001266e08363ad9f555bcb8257
Torben Jost <torbenjost@proton.me>
2024-11-26 16:31:55 +0100

    override toString method of the Library class

--- 261124_package/codeSession/src/main/java/neuefische/library.java
+++ 261124_package/codeSession/src/main/java/neuefische/library.java
@@ -1,5 +1,14 @@
 package neuefische;
 
+import java.util.Arrays;
+
 public class library {
     private Book[] books;
+
+    @Override
+    public String toString() {
+        return "library{" +
+                "books=" + Arrays.toString(books) +
+                '}';
+    }
 }
