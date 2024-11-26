1dee1e1517227a559629fc9a03469269dd07778f
Torben Jost <torbenjost@proton.me>
2024-11-26 11:28:17 +0100

    answer research questions

--- 261124_class_object/src/main/java/org/example/Main.java
+++ 261124_class_object/src/main/java/org/example/Main.java
@@ -32,19 +32,27 @@ public class Main {
     // CHALLENGES
 
 //    Research
+
 // I- Imagine you were developers for a video game that takes place in your room.
 // Choose an object in your room and think about how it could serve as an interactive element in the game.
-// What properties and actions could this object have? (each person chooses an object). inputfield
+// What properties and actions could this object have? (each person chooses an object.
+// siehe oben. "Lamp"
 
 
+//    II- Think of reasons why an object can have multiple constructors and give an example.
+    // Objects can be initialized using various types and numbers of parameters (e.g., partial vs. complete information).
 
-//    II- Think of reasons why an object can have multiple constructors and give an example. inputfield
-//    III-Can there be a class in Java that does not have a single object? inputfield
+//    III-Can there be a class in Java that does not have a single object?
+    // Yes. Any class exists on its own even if there isn't a single instance (object) of this class declared anywhere in the code.
+
+
 //    IV- Can there be an object in Java that does not belong to any class? inputfield
+    // No! As an object is the instance of a class. A class is constitutive to an object.
 
 
-//
 //    Coding: Classes and Objects
+
+    
 //    Create a class 'Car' together that represents the properties and methods of a car.
 //
 //    Step 1: Define the properties of a car, e.g. 'Brand', 'Model', 'Color', 'Year of Manufacture', etc.
