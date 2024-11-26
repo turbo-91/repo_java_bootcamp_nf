c63195875c20f3b2853f5ea767e357b8120a3417
Torben Jost <torbenjost@proton.me>
2024-11-26 17:13:15 +0100

    refactor Library and Book classes to add overrides

--- 261124_package/codeSession/src/main/java/de/neuefische/Library/Book.java
+++ 261124_package/codeSession/src/main/java/de/neuefische/Library/Book.java
@@ -1,5 +1,7 @@
 package de.neuefische.Library;
 
+import java.util.Objects;
+
 public class Book {
     private String title;
     private String author;
@@ -36,11 +38,24 @@ public class Book {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(isbn, book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, isbn);
+    }
+
+    @Override
     public String toString() {
         return "Book{" +
                 "title='" + title + '\'' +
                 ", author='" + author + '\'' +
-                ", isbn=" + isbn +
+                ", isbn='" + isbn + '\'' +
                 '}';
     }
+
 }
--- 261124_package/codeSession/src/main/java/de/neuefische/Library/Library.java
+++ 261124_package/codeSession/src/main/java/de/neuefische/Library/Library.java
@@ -1,6 +1,7 @@
 package de.neuefische.Library;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Library {
     private Book[] books;
@@ -16,10 +17,25 @@ public class Library {
     public void setBooks(Book[] books) {
         this.books = books;
     }
+
     @Override
     public String toString() {
         return "library{" +
                 "books=" + Arrays.toString(books) +
                 '}';
     }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(books);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return Objects.deepEquals(books, library.books);
+    }
+
+
 }
