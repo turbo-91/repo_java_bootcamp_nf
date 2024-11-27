cd6bd0c82b652efa6f64717ab9034f5380e3b726
Torben Jost <torbenjost@proton.me>
2024-11-27 15:13:39 +0100

    Write a static method 'incrementTotalCount' that increments the 'totalCount' by 1.

--- hh-java-24-5-static/src/main/java/de/neuefische/Counter.java
+++ hh-java-24-5-static/src/main/java/de/neuefische/Counter.java
@@ -3,4 +3,10 @@ package de.neuefische;
 public class Counter {
     public static int totalCount = 0;
     public int instanceCount = 0;
+
+    public static void incrementTotalCount() {
+        totalCount++;
+    }
+
+   
 }
