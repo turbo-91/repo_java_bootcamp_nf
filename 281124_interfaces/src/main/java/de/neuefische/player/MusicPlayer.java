0debdc7fb95b773608454f5d269f00432a8d3533
Torben Jost <torbenjost@proton.me>
2024-11-28 11:21:22 +0100

    refactor play methods

--- 281124_interfaces/src/main/java/de/neuefische/player/MusicPlayer.java
+++ 281124_interfaces/src/main/java/de/neuefische/player/MusicPlayer.java
@@ -3,5 +3,6 @@ package de.neuefische.player;
 public class MusicPlayer implements Playable {
     @Override
     public void play() {
+        System.out.println("Playing house music...");
     }
 }
--- 281124_interfaces/src/main/java/de/neuefische/player/VideoPlayer.java
+++ 281124_interfaces/src/main/java/de/neuefische/player/VideoPlayer.java
@@ -3,6 +3,6 @@ package de.neuefische.player;
 public class VideoPlayer implements Playable {
     @Override
     public void play() {
-        
+        System.out.println("Playing 90ies Star Trek episodes...");
     }
 }
