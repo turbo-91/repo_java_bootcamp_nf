fa261443057c7728d14f1edf37d8071d8103d893
Torben Jost <torbenjost@proton.me>
2024-11-28 11:28:09 +0100

    create class 'MediaController' containing method 'playMedia'

---
+++ 281124_interfaces/src/main/java/de/neuefische/player/MediaController.java
@@ -0,0 +1,12 @@
+package de.neuefische.player;
+
+public class MediaController implements Playable {
+    @Override
+    public void play() {
+
+    }
+
+    public void playMedia(Playable playable) {
+        play();
+    }
+}
