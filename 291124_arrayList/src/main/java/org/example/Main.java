ab94d0fd401a1a989e59738eb3d6dec67d2ca74e
Torben Jost <torbenjost@proton.me>
2024-11-29 11:52:53 +0100

    Create a Java List for elements of type Student and add multiple students

--- 291124_arrayList/src/main/java/org/example/Main.java
+++ 291124_arrayList/src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student("John", "Doe", 101));
+        students.add(new Student("Jane", "Smith", 102));
+        students.add(new Student("Emily", "Johnson", 103));
     }
 }
