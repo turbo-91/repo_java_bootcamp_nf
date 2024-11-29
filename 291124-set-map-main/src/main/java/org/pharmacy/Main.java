ac66133e4e5e8fefd852caf771c6bd6c6e1e2c1a
Torben Jost <torbenjost@proton.me>
2024-11-29 16:45:06 +0100

    Create a class 'Medication' with the methods 'getName', 'getPrice', and 'getAvailability'

---
+++ 291124-set-map-main/src/main/java/org/pharmacy/Main.java
@@ -0,0 +1,14 @@
+package org.pharmacy;
+
+import org.example.ShoppingItem;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
+
