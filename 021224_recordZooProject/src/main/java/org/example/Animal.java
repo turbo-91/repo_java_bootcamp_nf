75045ee015306d476407e1ef2b34e4bebda91272
Torben Jost <torbenjost@proton.me>
2024-12-02 11:46:05 +0100

    add withers

--- 021224_recordZooProject/src/main/java/org/example/Animal.java
+++ 021224_recordZooProject/src/main/java/org/example/Animal.java
@@ -1,4 +1,21 @@
 package org.example;
 
 public record Animal(String ID, String name, String species, int age) {
+
+    public Animal withID(String ID) {
+        return new Animal(ID, name, species, age);
+    }
+
+    public Animal withName(String name) {
+        return new Animal(name, this.name, this.species, this.age);
+    }
+
+    public Animal withSpecies(String species) {
+        return new Animal(this.ID, this.name, species, this.age);
+    }
+
+    public Animal withAge(int age) {
+        return new Animal(this.ID, this.name, this.species, this.age);
+    }
+
 }
