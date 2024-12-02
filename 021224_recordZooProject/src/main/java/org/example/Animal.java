a9d03981c56cf6346e635ab8761a3df207274563
Torben Jost <torbenjost@proton.me>
2024-12-02 11:56:55 +0100

    create owner record and give each animal an owner

--- 021224_recordZooProject/src/main/java/org/example/Animal.java
+++ 021224_recordZooProject/src/main/java/org/example/Animal.java
@@ -1,21 +1,25 @@
 package org.example;
 
-public record Animal(String ID, String name, String species, int age) {
+public record Animal(String ID, String name, String species, int age, Owner owner) {
 
     public Animal withID(String ID) {
-        return new Animal(ID, name, species, age);
+        return new Animal(ID, name, species, age, owner);
     }
 
     public Animal withName(String name) {
-        return new Animal(name, this.name, this.species, this.age);
+        return new Animal(ID, name, species, age, owner);
     }
 
     public Animal withSpecies(String species) {
-        return new Animal(this.ID, this.name, species, this.age);
+        return new Animal(ID, name, species, age, owner);
     }
 
     public Animal withAge(int age) {
-        return new Animal(this.ID, this.name, this.species, this.age);
+        return new Animal(ID, name, species, age, owner);
+    }
+
+    public Animal withOwner(Owner owner) {
+        return new Animal(ID, name, species, age, owner);
     }
 
 }
--- 021224_recordZooProject/src/main/java/org/example/Main.java
+++ 021224_recordZooProject/src/main/java/org/example/Main.java
@@ -3,10 +3,15 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        Animal cat = new Animal("1A", "kitty", "cat", 4);
-        Animal dog = new Animal("2A", "rufus", "dog", 7);
-        Animal donkey = new Animal("3A", "dirk", "donkey", 12);
-        Animal pig = new Animal("4A", "ms piggy", "pig", 5);
+        Owner tim = new Owner("time", 55, "Biebricher Straße");
+        Owner eva = new Owner("eva", 35, "Landsberger Allee");
+        Owner adam = new Owner("adam", 25, "Bergmannstraße");
+        Owner tom = new Owner("tom", 44, "Hermannstraße");
+
+        Animal cat = new Animal("1A", "kitty", "cat", 4, tim);
+        Animal dog = new Animal("2A", "rufus", "dog", 7, eva);
+        Animal donkey = new Animal("3A", "dirk", "donkey", 12, adam);
+        Animal pig = new Animal("4A", "ms piggy", "pig", 5, tom);
 
         System.out.println(pig.toString());
         System.out.println(cat.equals(dog));
