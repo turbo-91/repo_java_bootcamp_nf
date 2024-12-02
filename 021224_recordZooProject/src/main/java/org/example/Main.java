58d0fdf2520a11e658d727484c590703bd4c3c5f
Torben Jost <torbenjost@proton.me>
2024-12-02 11:49:57 +0100

    create multiple animal instances

--- 021224_recordZooProject/src/main/java/org/example/Main.java
+++ 021224_recordZooProject/src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Animal cat = new Animal("1A", "kitty", "cat", 4);
+        Animal dog = new Animal("2A", "rufus", "dog", 7);
+        Animal donkey = new Animal("3A", "dirk", "donkey", 12);
+        Animal pig = new Animal("4A", "ms piggy", "pig", 5);
+
+        System.out.println(pig.toString());
+        System.out.println(cat.equals(dog));
+
     }
 }
