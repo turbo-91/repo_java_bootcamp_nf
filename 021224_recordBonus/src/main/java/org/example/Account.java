12ada003b6a0e8a2ebc0a1a44f941463a226b30e
Torben Jost <torbenjost@proton.me>
2024-12-05 10:14:32 +0100

    commit method to deposit money into account

--- 021224_recordBonus/src/main/java/org/example/Account.java
+++ 021224_recordBonus/src/main/java/org/example/Account.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Account {
 private String accountNumber;
@@ -12,4 +13,56 @@ private Client client;
         this.balance = balance;
         this.client = client;
     }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(accountNumber, account.accountNumber) && Objects.equals(balance, account.balance) && Objects.equals(client, account.client);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNumber, balance, client);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", balance=" + balance +
+                ", client=" + client +
+                '}';
+    }
+
+    public BigDecimal depositMoney(BigDecimal amount, BigDecimal balance) {
+        BigDecimal newBalance = balance.add(amount);
+        setBalance(newBalance);
+        return newBalance;
+    };
+
 }
--- 021224_recordBonus/src/main/java/org/example/Main.java
+++ 021224_recordBonus/src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Client testClient1 = new Client("Torben", "Jost", 123);
+        Client testClient2 = new Client("David", "Jost", 456);
+
+        Account testAccount1 = new Account("123tor", new BigDecimal("1256.23"),testClient1);
+        Account testAccount2 = new Account("456dav", new BigDecimal("400.00"),testClient2);
+
+        System.out.println( testAccount1.getClient().FirstName() + "'s balance before Deposit:");
+        System.out.println( testAccount1.getBalance());
+        testAccount1.depositMoney(new BigDecimal("200.00"), testAccount1.getBalance());
+        System.out.println( testAccount1.getClient().FirstName() + "'s balance after Deposit:");
+        System.out.println( testAccount1.getBalance());
+
+
     }
 }
