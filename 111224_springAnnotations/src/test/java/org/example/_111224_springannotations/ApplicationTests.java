1f5685a2200b37353aa960aa3fe5f369d67f39c0
Torben Jost <torbenjost@proton.me>
2024-12-11 16:25:57 +0100

    setup new springBoot project

---
+++ 111224_springAnnotations/src/test/java/org/example/_111224_springannotations/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example._111224_springannotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
