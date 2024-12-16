4f5b224d44a14e47d2e85b332ab68b6654553897
Torben Jost <torbenjost@proton.me>
2024-12-16 12:02:30 +0100

    do research

--- 131224_springService/src/test/java/org/example/_121224_asterisk/research.java
+++ 131224_springService/src/test/java/org/example/_121224_asterisk/research.java
@@ -3,6 +3,28 @@ package org.example._121224_asterisk;
 public class research {
 }
 
+// RESEARCH 16-12-24
+
+// What are the main differences between an integration test and a unit test?
+// Unit tests focus on testing individual components in isolation, while integration tests validate the interaction
+// between multiple components or systems.
+
+// _______________________________________________
+
+// How can you use MockMvc to verify that the HTTP status of a request is 403?
+// Use mockMvc.perform(request).andExpect(status().isForbidden()); to verify that the HTTP status of a request is 403.
+
+// _______________________________________________
+
+// How can you expect a JSON in the response body using MockMvc:
+//That has the property "name" with the value "Max"
+//That has the property "age" with the value 10?
+
+// Use mockMvc.perform(request).andExpect(jsonPath("$.name").value("Max")).andExpect(jsonPath("$.age").value(10)); to
+// assert JSON properties with specified values in the response body.
+
+// RESEARCH 13-12-24
+
 // What is a mock, and why is mocking important in software development?
 // A mock is a simulated object that mimics the behavior of real objects in controlled ways, allowing developers to
 // isolate and test specific parts of the application. Mocking is important because it ensures tests are faster, more
