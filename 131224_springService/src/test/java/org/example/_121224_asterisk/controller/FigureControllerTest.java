aa68bdfab7a0c13eb0d298ac0c2a42ff332d3b66
Torben Jost <torbenjost@proton.me>
2024-12-16 12:46:51 +0100

    test getAll method in controller

--- 131224_springService/src/test/java/org/example/_121224_asterisk/controller/FigureControllerTest.java
+++ 131224_springService/src/test/java/org/example/_121224_asterisk/controller/FigureControllerTest.java
@@ -1,4 +1,52 @@
+package org.example._121224_asterisk.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
 import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class FigureControllerTest {
-  
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getAll_shouldReturnEmptyList_whenCalledInitially() throws Exception {
+       //GIVEN
+
+        // THEN & WHEN
+        mockMvc.perform(get("/api/asterix/characters"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void createFigure() {
+    }
+
+    @Test
+    void updateFigure() {
+    }
+
+    @Test
+    void deleteFigure() {
+    }
 }
