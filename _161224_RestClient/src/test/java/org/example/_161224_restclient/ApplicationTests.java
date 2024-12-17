31364ecdaf7280f7b486b4f743f0374e12af301b
Torben Jost <torbenjost@proton.me>
2024-12-17 15:49:56 +0100

    setup project

---
+++ _161224_RestClient/src/test/java/org/example/_161224_restclient/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example._161224_restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
