38cf3bd0a29b5cceea6a6f89051c5e2c5c3d57f1
Torben Jost <torbenjost@proton.me>
2025-01-13 12:11:39 +0100

    create form with inputs for most important character attributes

--- 130125_react_form/src/components/createCharacter.tsx
+++ 130125_react_form/src/components/createCharacter.tsx
@@ -1,7 +1,44 @@
+import {useState} from "react";
 
 
-export default function createCharacter() {
+export default function CreateCharacter() {
+
+    const [id, setId] = useState<string>("")
+    const [name, setName] = useState<string>("")
+    const [species, setSpecies] = useState<string>("")
+    const [image, setImage] = useState<string>("")
+
     return (
-       <></>
-    );
+        <>
+            <form>
+                <label>Id:
+                    <input
+                        type={"text"}
+                        value={id}
+                        onChange={(event) => setId(event.target.value)}/>
+                </label>
+                <br/>
+                <label>Name:
+                    <input
+                        type={"text"}
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}/>
+                </label>
+                <br/>
+                <label>Species:
+                    <input
+                        type={"text"}
+                        value={species}
+                        onChange={(event) => setSpecies(event.target.value)}/>
+                </label>
+                    <br/>
+                    <label>Image:
+                        <input
+                            type={"text"}
+                            value={image}
+                            onChange={(event) => setImage(event.target.value)}/>
+                    </label>
+            </form>
+        </>
+)
 }
